09195e52d9150984f5f52be6f7b27163968d33d4
Ashurov <roman.ash.dm@gmail.com>
2026-09-17 22:16:59 +0300

    wip2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import Post from "../ui/post/post";
 import Cards from "../ui/cards/cards";
-import pic1 from "../assets/images.jpg";
+import pic1 from "/images.jpg"
 import Footer from "../ui/footer/footer";
 
 export default function HomePage() {
