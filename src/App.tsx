a11805833019d5e5aa869b31751f640b9fd180a8
1nterFekT <lavamishals@gmail.com>
2026-09-17 19:08:51 +0300

    Hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import HomePage from "./pages/home"
-import WorksPage from "./pages/works"
-import ExamplePage from "./pages/example"
-import Nav from "./ui/nav/nav"
+import HomePage from "./pages/home";
+import WorksPage from "./pages/works";
+import ExamplePage from "./pages/example";
+import Nav from "./ui/nav/nav";
 
-type Page = 'home' | 'works' | 'example'
+type Page = "home" | "works" | "example";
 
 export default function App() {
-  const [page, setPage] = useState<Page>('example')
+    const [page, setPage] = useState<Page>("example");
 
-  const pages = {
-    home: <HomePage />,
-    works: <WorksPage />,
-    example: <ExamplePage />
-  }
+    const pages = {
+        home: <HomePage />,
+        works: <WorksPage />,
+        example: <ExamplePage />,
+    };
 
-  return (
-    <>
-      <Nav setPage={setPage} active={page} />
-      {pages[page]}
-    </>
-  )
+    return (
+        <>
+            <Nav setPage={setPage} active={page} />
+            {pages[page]}
+        </>
+    );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,44 @@
-import Post from "../ui/post/post"
+import Post from "../ui/post/post";
 
 export default function HomePage() {
     return (
         <>
-            <h1>Hi, I am John, Creative Technologist</h1>
             <section>
-                <h2>Recent posts</h2>
-                <div className="row">
-                    <Post title="Making a design system from scratch" date="12 Feb 2020" />
-                    <Post title="Creating pixel perfect icons in Figma" date="14 May 2026" />
+                <div className="container hero">
+                    <div>
+                        <h1>Hi, I am John, Creative Technologist</h1>
+                        <p>
+                            Amet minim mollit non deserunt ullamco est sit
+                            aliqua dolor do amet sint. Velit officia consequat
+                            duis enim velit mollit. Exercitation veniam
+                            consequat sunt nostrud amet.
+                        </p>
+                        <a className="hero__download" href="#">
+                            Download Resume
+                        </a>
+                    </div>
+                    <img
+                        className="hero__avatar"
+                        src="src\assets\hero.png"
+                        alt="Hero avatar"
+                    />
+                </div>
+            </section>
+            <section>
+                <div className="container">
+                    <h2>Recent posts</h2>
+                    <div className="row">
+                        <Post
+                            title="Making a design system from scratch"
+                            date="12 Feb 2020"
+                        />
+                        <Post
+                            title="Creating pixel perfect icons in Figma"
+                            date="14 May 2026"
+                        />
+                    </div>
                 </div>
             </section>
         </>
-    )
+    );
 }
