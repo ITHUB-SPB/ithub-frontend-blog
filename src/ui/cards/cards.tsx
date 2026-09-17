f1edb9e979036d4360a9558e2f02d1a0100c02be
Ashurov <roman.ash.dm@gmail.com>
2026-09-18 00:19:17 +0300

    wip4

--- src/ui/cards/cards.tsx
+++ src/ui/cards/cards.tsx
@@ -10,11 +10,11 @@ type CardsProps = {
 export default function Cards(props: CardsProps) {
   return (
     <article className="card">
-      <img src={props.images} alt="" className="card__image" />
+      <img className="card__image" src={props.images} alt="" />
       <div className="card__content">
-        <h3>{props.title}</h3>
+        <h3 className="card__title">{props.title}</h3>
         <section className="card__meta">
-          <span className="card__meta__date">{props.date}</span>
+            <span className="card__meta__date">{props.date}</span>
           <span className="card__meta__annotation">{props.annotation}</span>
         </section>
         <p>
