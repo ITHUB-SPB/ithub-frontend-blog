c18841662ad73cc94e20509ad3b73b284198f81f
Ashurov <roman.ash.dm@gmail.com>
2026-09-17 23:21:02 +0300

    fix footer

--- src/ui/cards/cards.tsx
+++ src/ui/cards/cards.tsx
@@ -14,8 +14,8 @@ export default function Cards(props: CardsProps) {
       <div className="card__content">
         <h3>{props.title}</h3>
         <section className="card__meta">
-          <span className="card__meta__item">{props.date}</span>
-          <span className="card__meta__item">{props.annotation}</span>
+          <span className="card__meta__date">{props.date}</span>
+          <span className="card__meta__annotation">{props.annotation}</span>
         </section>
         <p>
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
